46b120a38c96b01b3ee877f58dad099505679137
0xbadad <aels@users.noreply.github.com>
2023-02-27 08:53:48 +0200

    Update madcatmailer.py

--- mass-mailer/madcatmailer.py
+++ mass-mailer/madcatmailer.py
@@ -192,18 +192,14 @@ def expand_macros(text, subs):
 		text = text.replace(macro, random.choice(macro[2:-2].split('|')))
 	return text
 
-def get_read_receipt_headers(mail_from, mail_to):
-	global no_read_receipt_for
-	if re.findall(no_read_receipt_for, mail_to.lower()):
-		return ''
-	else:
-		receipt_headers =f'Disposition-Notification-To: {mail_from}\n'
-		receipt_headers+=f'Generate-Delivery-Report: {mail_from}\n'
-		receipt_headers+=f'Read-Receipt-To: {mail_from}\n'
-		receipt_headers+=f'Return-Receipt-Requested: {mail_from}\n'
-		receipt_headers+=f'Return-Receipt-To: {mail_from}\n'
-		receipt_headers+=f'X-Confirm-reading-to: {mail_from}\n'
-		return receipt_headers
+def get_read_receipt_headers(mail_from):
+	receipt_headers =f'Disposition-Notification-To: {mail_from}\n'
+	receipt_headers+=f'Generate-Delivery-Report: {mail_from}\n'
+	receipt_headers+=f'Read-Receipt-To: {mail_from}\n'
+	receipt_headers+=f'Return-Receipt-Requested: {mail_from}\n'
+	receipt_headers+=f'Return-Receipt-To: {mail_from}\n'
+	receipt_headers+=f'X-Confirm-reading-to: {mail_from}\n'
+	return receipt_headers
 
 def str_ljust(string, length):
 	is_inside_tag = False
@@ -232,7 +228,7 @@ def smtp_connect(smtp_server, port, user, password):
 	return server_obj
 
 def smtp_sendmail(server_obj, smtp_server, smtp_user, mail_str):
-	global config
+	global config, no_read_receipt_for
 	mail_redirect_url = random.choice(config['redirects_list'])
 	subs = [mail_str, smtp_user, mail_redirect_url]
 	mail_to = extract_email(mail_str)
@@ -260,7 +256,8 @@ def smtp_sendmail(server_obj, smtp_server, smtp_user, mail_str):
 	headers+= 'X-Mailer: Microsoft Office Outlook, Build 10.0.5610\n'
 	headers+= 'X-MimeOLE: Produced By Microsoft MimeOLE V6.00.2800.1441\n'
 	headers+= 'Received: '+get_random_name()+'\n'
-	headers+= get_read_receipt_headers(smtp_from, mail_to)
+	if not re.findall(no_read_receipt_for, mail_to.lower()):
+		headers+= get_read_receipt_headers(smtp_from)
 	message_raw = headers + message.as_string()
 	server_obj.sendmail(smtp_from, mail_to, message_raw)
 
